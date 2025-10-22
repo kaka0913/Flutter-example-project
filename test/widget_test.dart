e42df03ef7b230193ba8f679d293f3c367187cb6
kaka0913 <kabuyuu0913@gmail.com>
2025-10-22 14:43:22 +0900

    actions設定

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+// This is a basic Flutter widget test.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:work/main.dart';
+
+void main() {
+  testWidgets('MainApp displays Hello World', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
